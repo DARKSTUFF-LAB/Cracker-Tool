13381d5fcc186e995034b3199fa53adb80f4c8da
@DARK_STUFF <109729013+DARKSTUFF-LAB@users.noreply.github.com>
2023-04-01 16:38:22 +0500

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -11,7 +11,7 @@ pip install pyfiglet
 pip install bs4
 pip install lolcat
 #pip install -i https://test.pypi.org/simple/ cracker-say
-#pip install git+https://github.com/cracker911181/cracker_say
+#pip install git+https://github.com/DARKSTUFF-LAB/cracker_say
 pip install cracker-saying
 pip install cowsay 
 pip install youtube_dl
